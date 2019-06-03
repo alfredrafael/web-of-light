3eb8c11f95eb55207ea7d1b3454370ea762dd7aa
alfredrafael <alfredrafael@gmail.com>
2019-06-03 11:25:00 -0400

    BoilerPlate worthy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Exploratory from './components/Exploratory';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Row } from "reactstrap";
+import Exploratory from "./components/Exploratory";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border-radius: 4px;
+  border: none;
+  padding: 1rem;
+  margin: 1rem 2rem;
+  font-size: 1rem;
+  font-family: "Colfax Regular", "Helvetica Neue", HelveticaNeue, Helvetica,
+    sans-serif;
+  background-color: #0076fe;
+  color: #fff;
+  cursor: pointer;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Exploratory />
-      </div>
+      <Router>
+        <Row style={{ background: "blue" }}>
+          <Container style={{ background: "rgb(123, 123, 228)" }}>
+            
+            <img src={logo} className="App-logo" alt="logo" />
+           
+            This is <strong>App.js</strong> &nbsp;<i class="fas fa-home" style={{ color: "white" }} /><br />
+           
+            If this background is blue, layed out as a row and a container, your <strong>"reactstrap"</strong> is working. <br />
+           
+            If you see a home icon on this component, your <strong>"fontAwesome"</strong> is working" <br />
+         
+            If you change the url to /extra, or click <Link to="/extra"> here </Link> it works, your <strong>"react-router"</strong>is working <br />
+            
+            If <Button>this</Button> looks like a button, then your "styled components" is working <br/>
+
+            <Route path="/exploratory" component={Exploratory} />
+          
+          </Container>
+        </Row>
+      </Router>
     );
   }
 }
